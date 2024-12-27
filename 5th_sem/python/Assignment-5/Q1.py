6cdf1144d746cfcfefa528e40512087db181abc2
Nabin Kumar Shaw <122195307+nabinkumarshaw@users.noreply.github.com>
2024-12-28 00:37:19 +0530

    Add files via upload

---
+++ 5th_sem/python/Assignment-5/Q1.py
@@ -0,0 +1,13 @@
+'''Write aprogramtoaccept student name and marks fromthe user and create a dictionary. Also, display
+ student marks by taking student name as input.'''
+ 
+ import numpy as np
+
+array_ones = np.ones((2, 3))
+print(array_ones)
+
+array_zeros = np.zeros((3, 3))
+print(array_zeros)
+
+array_sevens = np.full((2, 5), 7)
+print(array_sevens)
