5b24f9b3b3884b0f28c6ae519cab811469707b1d
Nabin Kumar Shaw <122195307+nabinkumarshaw@users.noreply.github.com>
2024-12-29 22:18:22 +0530

    Update Q9.py

--- 5th_sem/python/Assignment-5/Q9.py
+++ 5th_sem/python/Assignment-5/Q9.py
@@ -7,8 +7,3 @@ string = input("Enter a string: ")
 occurrences = {char: string.count(char) for char in string}
 print(occurrences)
 
-'''
-Output ->
-Enter a string: Hello, Its DevSom404
-{'H': 1, 'e': 2, 'l': 2, 'o': 2, ',': 1, ' ': 2, 'I': 1, 't': 1, 's': 1, 'D': 1, 'v': 1, 'S': 1, 'm': 1, '4': 2, '0': 1}
-'''
